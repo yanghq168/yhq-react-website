85c1a46c177eb6323a80a4f7c87be071c2c3a0bf
yanghq168 <32892406+yanghq168@users.noreply.github.com>
2018-07-11 16:00:20 +0800

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,16 +17,16 @@ export default class Header extends Component {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a className="navbar-brand brand-name" href="#">珠峰前端微课堂</a>
+            <a className="navbar-brand brand-name" href="#">杨洪权xx公司</a>
           </div>
           <div id="navbar" className="navbar-collapse collapse">
             <ul className="nav navbar-nav">
               <li className="active"><a href="/">首页</a></li>
-              <li><Link to="/react">React</Link></li>
-              <li><Link to="/es6">ECMAScript6</Link></li>
-              <li><Link to="/webpack">Webpack</Link></li>
-              <li><Link to="/redux">Redux</Link></li>
-              <li><Link to="/babel">Babel</Link></li>
+              <li><Link to="/react">展示页1</Link></li>
+              <li><Link to="/es6">展示页2</Link></li>
+              <li><Link to="/webpack">展示页3</Link></li>
+              <li><Link to="/redux">展示页4</Link></li>
+              <li><Link to="/babel">展示页5</Link></li>
               <li><Link to="/about">关于我们</Link></li>
             </ul>
           </div>
